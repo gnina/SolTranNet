966837d0da8d11a67bdee5451521f54222d6e5be
Paul Francoeur <33321268+francoep@users.noreply.github.com>
2021-03-10 11:48:22 -0500

    Update __init__.py

--- soltrannet/__init__.py
+++ soltrannet/__init__.py
@@ -23,7 +23,7 @@ self.model=tmp
 
 def run():
     parser=argparse.ArgumentParser(description="Run SolTranNet aqueous solubility predictor")
-    parser.add_argument('-i','--input',required=True,nargs='?',type=argparse.FileType('r'),default=sys.stdin,help='PATH to the file containing the SMILES you wish to use. Assumes the content is 1 SMILE per line.')
+    parser.add_argument('-i','--input',nargs='?',type=argparse.FileType('r'),default=sys.stdin,help='PATH to the file containing the SMILES you wish to use. Assumes the content is 1 SMILE per line.')
     parser.add_argument('-o','--output',nargs='?',type=argparse.FileType('w'),default=sys.stdout,help='Name of the output file.')
     parser.add_argument('--batchsize',default=32,help='Batch size for the data loader. Defaults to 32.')
     args=parser.parse_args()
