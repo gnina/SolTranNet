75f377097b2c772675bf37ad6be42589e94e4fdc
Paul Francoeur <33321268+francoep@users.noreply.github.com>
2021-03-12 15:12:44 -0500

    Updating command line script
    Added a --cpu_predict option to the command line utility to force cpu predictions, even if the GPU is available.
    
    Modified the unittest accordingly

--- soltrannet/__init__.py
+++ soltrannet/__init__.py
@@ -8,7 +8,8 @@ def _parse_args():
     parser.add_argument('input',nargs='?',type=argparse.FileType('r'),default=sys.stdin,help='PATH to the file containing the SMILES you wish to use. Assumes the content is 1 SMILE per line.')
     parser.add_argument('output',nargs='?',type=argparse.FileType('w'),default=sys.stdout,help='Name of the output file.')
     parser.add_argument('--batchsize',default=32,type=int,help='Batch size for the data loader. Defaults to 32.')
-    parser.add_argument('--cpus',default=multiprocessing.cpu_count(),type=int,help='Number of CPU cores to use. Defaults to use all available cores. Pass 0 to only run on 1 CPU.')
+    parser.add_argument('--cpus',default=multiprocessing.cpu_count(),type=int,help='Number of CPU cores to use for the data loader. Defaults to use all available cores. Pass 0 to only run on 1 CPU.')
+    parser.add_argument('--cpu_predict',action='store_true',help='Flag to force the predictions to be made on only the CPU. Default behavior is to use GPU if available.')
 
     args=parser.parse_args()
 
@@ -17,7 +18,10 @@ def _parse_args():
 def _run(args):
 
     smiles=[x.rstrip() for x in args.input]
-    predictions=predict(smiles,batch_size=args.batchsize,num_workers=args.cpus)
+    if args.cpu_predict:
+        predictions=predict(smiles,batch_size=args.batchsize,num_workers=args.cpus,device=torch.device('cpu'))
+    else:
+        predictions=predict(smiles,batch_size=args.batchsize,num_workers=args.cpus)
     for pred, smi, warn in predictions:
         args.output.write(f'{smi},{pred:.3f},{warn}\n')
 
--- soltrannet/tests/test_commandline.py
+++ soltrannet/tests/test_commandline.py
@@ -9,6 +9,6 @@ class TestCommandLine(TestCase):
             correct='c1ccccc1,-1.053,\nc1ccccc1 .ignore,-1.053,\nCn1cnc2n(C)c(=O)n(C)c(=O)c12,-1.132,\n[Zn+2],-6.882,Other-typed Atom(s) Detected Prediction less reliable\n[Na+].[Cl-],-0.169,Salt Other-typed Atom(s) Detected Prediction less reliable\n'
 
             args=argparse.Namespace(input=['c1ccccc1','c1ccccc1 .ignore','Cn1cnc2n(C)c(=O)n(C)c(=O)c12','[Zn+2]','[Na+].[Cl-]'],
-                                                                                             output=buf,batchsize=5, cpus=0)
+                                                                                             output=buf,batchsize=5, cpus=0,cpu_predict=None)
             _run(args)
             self.assertEqual(buf.getvalue(),correct)
